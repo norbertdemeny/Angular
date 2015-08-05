5531ddee6c08c80f472dccfe46222a51ea60458c
norbertdemeny <dudukid2009@yahoo.com>
2015-08-05 15:22:34 +0300

    search is working

---
+++ Angular/js/SearchinArray.js
@@ -0,0 +1,56 @@
+/**
+ * Created by norbertdemeny on 8/5/2015.
+ */
+(function() {
+    var app = angular.module('app', []);
+
+    app.controller('AppCtrl', function ($scope) {
+
+
+        $scope.categories = [{
+            type: 'dir',
+            name: 'app',
+            children: [{
+                type: 'file',
+                name: 'index.html'
+            }, {
+                type: 'dir',
+                name: 'js',
+                children: [{
+                    type: 'file',
+                    name: 'main.js'
+                }, {
+                    type: 'file',
+                    name: 'app.js'
+                }, {
+                    type: 'file',
+                    name: 'misc.js'
+                }, {
+                    type: 'dir',
+                    name: 'vendor',
+                    children: [{
+                        type: 'file',
+                        name: 'jquery.js'
+                    }, {
+                        type: 'file',
+                        name: 'underscore.js'
+                    }]
+                }]
+            },
+                {
+                    type: 'dir',
+                    name: 'css',
+                    children: [{
+                        type: 'file',
+                        name: 'reset.css'
+                    }, {
+                        type: 'file',
+                        name: 'main.css'
+                    }]
+                }]
+
+        }]
+
+    });
+})();
+
