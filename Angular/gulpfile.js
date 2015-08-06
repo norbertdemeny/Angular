2b331f1eade5c1842222c06a5d7cd36b6ade3d50
norbertdemeny <dudukid2009@yahoo.com>
2015-08-06 15:34:17 +0300

    Delete gulpfile.js

--- Angular/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-
-gulp.task('js', function () {
-    return gulp.src('./js/**/*.js')
-        .pipe(concat('bundle.js'))
-        .pipe(gulp.dest('./js/'));
-});
